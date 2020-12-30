0fe9e954c74c82fa002864c5c7eda018000cb7ac
Take327 <gatewayseed2@gmail.com>
2020-12-30 15:12:27 +0900

    initial commit

---
+++ app/app.js
@@ -0,0 +1,131 @@
+const express = require('express');
+const app = express();
+
+const sqlite3 = require('sqlite3');
+const dbPath = 'app/db/database.sqlite3';
+
+const bodyParser = require('body-parser');
+
+const path = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log("Listen on port:" + port);
+
+
+// Get all users
+app.get('/api/v1/users', (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+
+    db.all('SELECT * FROM users', (err, rows) => {
+        res.json(rows);
+    })
+
+    db.close();
+});
+
+// Get a user
+app.get('/api/v1/users/:id', (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+    //リクエストのパラメータ（id）を取得する。
+    const id = req.params.id;
+
+    db.get(`SELECT * FROM users WHERE id =${id}`, (err, row) => {
+        res.json(row);
+    })
+
+    db.close();
+});
+
+// Search users matching keyword
+app.get('/api/v1/search', (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+    //検索キーワードを取得
+    //http:localhost:3000/api/v1/search?q=検索文字
+    const keyword = req.query.q;
+
+    db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+        res.json(rows);
+    })
+
+    db.close();
+});
+
+const run = async (sql, db, res, message) => {
+    return new Promise((resolve, reject) => {
+        db.run(sql, (err) => {
+            if (err) {
+                res.status(500).send(err);
+                return reject();
+            } else {
+                res.json({ message: message });
+                return resolve();
+            }
+        })
+
+    });
+}
+
+// Create a new user
+app.post('/api/v1/users', async (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+
+    const name = req.body.name;
+    const profile = req.body.profile ? req.body.profile : "";
+    const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : "";
+
+    await run(`INSERT INTO users (name,profile,date_of_birth) VALUES("${name}","${profile}","${dateOfBirth}")`,
+        db, res, "新規ユーザーを作成しました。");
+
+    db.close();
+
+});
+
+// Update user data
+app.put('/api/v1/users/:id', async (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+    const id = req.params.id;
+
+    //現在のユーザー情報を取得する。
+    db.get(`SELECT * FROM users WHERE id =${id}`, async (err, row) => {
+        const name = req.body.name ? req.body.name : row.name;
+        const profile = req.body.profile ? req.body.profile : row.profile;
+        const dateOfBirth = req.body.date_of_birth ? req.body.date_of_birth : row.date_of_birth;
+
+        await run(
+            `UPDATE users SET name="${name}",profile="${profile}",date_of_birth="${dateOfBirth} WHERE id=${id}`,
+            db, res, "ユーザー情報を更新しました。"
+        );
+    });
+
+    db.close();
+
+});
+
+// delete user data
+app.delete('/api/v1/users/:id', async (req, res) => {
+    //データベースに接続する。
+    const db = new sqlite3.Database(dbPath);
+    const id = req.params.id;
+
+    //ユーザー情報を削除する。
+    await run(
+        `DELETE FROM users WHERE id=${id}`,
+        db, res, "ユーザー情報を削除しました。"
+    );
+
+    db.close();
+
+});
+
+
---
+++ app/public/js/index.js
@@ -0,0 +1,10 @@
+const indexModule = (() => {
+
+    document.getElementById('search-btn').addEventListener('click', () => {
+        return searchModule.searchUsers();
+    })
+
+    return usersModule.fetchAllUsers();
+})()
+
+
